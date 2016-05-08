ccd1b8f51e066f65c3e345fe4ca4da756a0f1fb5
danman113 <danman113@msn.com>
2016-05-07 22:22:05 -0700

    Added swimming, seamless worlds, post processing effects, gamepad support

--- tests/collision.js
+++ tests/collision.js
@@ -1,37 +1,31 @@
 var worker = new Worker("../boundThreads.js");
 var option = {type:'create',size:40,scale:10,options:{x:1,y:1,z:1,resolution:512,seed:1}};
-var ready = false;
+var swimming = false;
 var grounds = {};
 worker.onmessage = function(e){
-	var size = 512;
-	// e.data.data.x/=3;
-	// e.data.data.y/=3;
-	var x = 1*(e.data.data.x*(size));
-	var y = 1*(e.data.data.y*(size));
-	console.log(e,x,y);
+	var size = 1024;
+	var scale = 200;
+	var x = 1*(Math.round(e.data.data.x)*(size));
+	var y = -1*(Math.round(e.data.data.y)*(size));
+	// console.log(e,x,y);
+	if(!grounds[Math.round(e.data.data.x)+"x"+Math.round(e.data.data.y)]) return false;
 	var ground = CreateGroundFrom2dArray(
 		e.data.data.x+"x"+e.data.data.y,
 		e.data.data.map,{
 			width:size, // width of the ground mesh (x axis)
 			height:size, // depth of the ground mesh (z axis)
-			subdivisions:40,  // number of subdivisions
+			subdivisions:60,  // number of subdivisions
 			minHeight:0,   // min height
-			maxHeight:100,  // max height
+			maxHeight:scale,  // max height
 		},
 		scene
 	);
-	for (var i = e.data.data.map.length - 1; i >= 0; i--) {
-		for (var j = e.data.data.map[i].length - 1; j >= 0; j--) {
-			c.fillStyle = "#"+(biome(e.data.data.map[i][j]).toString());
-			c.fillRect(j+e.data.data.x*512,i+e.data.data.y*512,0.5,0.5);
-		}
-	}
-	grounds[e.data.data.x+"x"+e.data.data.y].map = e.data.data.map;
-	grounds[e.data.data.x+"x"+e.data.data.y].mesh = ground;
+	grounds[Math.round(e.data.data.x)+"x"+Math.round(e.data.data.y)].map = e.data.data.map;
+	grounds[Math.round(e.data.data.x)+"x"+Math.round(e.data.data.y)].mesh = ground;
 	ground.material = new BABYLON.StandardMaterial("d", scene);
 	//ground.material.wireframe = true;
 	ground.material.diffuseColor = new BABYLON.Color3(0.3, 0.7, 0.3);
-	ground.material.backFaceCulling = false;
+	ground.material.backFaceCulling = true;
 
 	ground.position = new BABYLON.Vector3(x, -30, y);
 	ground.checkCollisions = true;
@@ -44,6 +38,8 @@ worker.onmessage = function(e){
 			if(z && (jumpProgress == 0))
 				velocity = 0;
 		};
+		camera.inputs.add(new BABYLON.FreeCameraGamepadInput());
+		camera.inputs.attached.gamepad.gamepadAngularSensibility = 50;
 	}
 	
 };
@@ -68,7 +64,6 @@ var createScene = function () {
 	stats.domElement.style.top = "0px";
 	// Lights
 	var light0 = new BABYLON.DirectionalLight("Omni", new BABYLON.Vector3(-2, -5, 2), scene);
-	var light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(2, -5, -2), scene);
 
 	// Need a free camera for collisions
 	scene.enablePhysics();
@@ -77,16 +72,15 @@ var createScene = function () {
 	initCamera(camera);
 	//Simple crate
 	window.box = BABYLON.Mesh.CreateBox("crate", 2, scene);
-	window.box2 = BABYLON.Mesh.CreateBox("crate2", 2, scene);
 	window.water = BABYLON.Mesh.CreatePlane("impact", 1, scene);
-	window.water.position  = new BABYLON.Vector3(0, -30, 0);
+	window.water.position  = new BABYLON.Vector3(0, 0, 0);
 	window.water.scaling = new BABYLON.Vector3(0xfff, 0xfff, 0xfff);
 	window.water.material = new BABYLON.StandardMaterial("Mat", scene);
 	window.water.material.diffuseColor = new BABYLON.Color3(0, 0.5, 1);
 	window.water.rotation.x = Math.PI/2;
-	window.water.position.y=25;
+	window.water.position.y=120;
+	window.water.material.backFaceCulling = false;
 	window.c = createCanvas();
-	box2.visibility=0;
 	box.scaling.x = 10;
 	box.scaling.y = 3;
 	box.scaling.z = 10;
@@ -114,6 +108,15 @@ var createScene = function () {
 	box.checkCollisions = true;
 	loadChunk(0,0);
 	enablePointerlock(scene);
+	var sepiaKernelMatrix = [
+                   0.393, 0.349, 0.272, 0, 0,
+                   0.769, 0.686, 0.534, 0, 0,
+                   0.189, 0.168, 0.131, 0, 0,
+                   0,     0,     0,     0, 0,
+                   0,     0,     0,     0, 0
+                ];
+	window.ppBlur = new BABYLON.ConvolutionPostProcess("Sepia", BABYLON.ConvolutionPostProcess.GaussianKernel , 1, camera, null, engine, true);
+	window.ppBlue = new BABYLON.ColorCorrectionPostProcess("color_correction", "/assets/LUT_MatrixBlue.png", 1.0, camera, null, engine, true);
 	return scene;
 };
 
@@ -139,10 +142,10 @@ var CreateGroundFrom2dArray = function (name, array, options, scene) {
     var buffer = new Uint8ClampedArray(bufferWidth*bufferHeight*4);
     for(var i = bufferHeight-1; i>=0;--i){
 		for(var j = bufferWidth-1; j>=0;--j){
-			buffer[bufferWidth*j*4+(i*4)] = array[i][j]*255;
-			buffer[bufferWidth*j*4+(i*4)+1] = array[i][j]*255;
-			buffer[bufferWidth*j*4+(i*4)+2] = array[i][j]*255;
-			buffer[bufferWidth*j*4+(i*4)+3] = 255;
+			buffer[bufferWidth*i*4+(j*4)] = array[i][j]*255;
+			buffer[bufferWidth*i*4+(j*4)+1] = array[i][j]*255;
+			buffer[bufferWidth*i*4+(j*4)+2] = array[i][j]*255;
+			buffer[bufferWidth*i*4+(j*4)+3] = 255;
 		}	
 	}
     var vertexData = BABYLON.VertexData.CreateGroundFromHeightMap({
@@ -213,17 +216,68 @@ var jump = (function(){
 })();
 
 function loadChunk(x,y){
-	// x*=3;
-	// y*=3;
 	if(grounds[x+"x"+y]){
 		return false;
 	} else {
 		grounds[x+"x"+y] = {x:x,y:y};
-		var option = {type:'create',size:40,scale:10,options:{x:x,y:y,z:1,resolution:512,seed:1}};
-		worker.postMessage(option);
+		var option = {type:'create',size:40,scale:10,options:{x:x,y:y,z:1+1/512,resolution:512,seed:1}};
+		setTimeout(function(){
+			worker.postMessage(option);
+			
+		},Math.floor(Math.random()*0xfff));
 		return grounds[x+"x"+y];
 	}
 
+}
+
+function eraseChunk(x,y){
+	if(grounds[x+"x"+y]){
+		if(grounds[x+"x"+y].mesh){
+			grounds[x+"x"+y].mesh.dispose(false);
+		}
+		delete grounds[x+"x"+y];
+	} else {
+		return false;
+	}
+}
+
+function streamMap(position,size){
+	var range = 2;
+	var ctX = Math.floor(1*(position.x/(size)));		//currentTile
+	var ctY = Math.floor(-1*(position.z/(size)));
+	loadChunk((ctX),(ctY));
+	console.log(ctX,ctY);
+	var sight = {};
+	sight[(ctX+'x'+ctY)] = 1;
+	for (var i = 1; i <=range; i++) {
+		loadChunk((ctX-i),(ctY-i));
+		loadChunk((ctX  ),(ctY-i));
+		loadChunk((ctX-i),(ctY  ));
+		loadChunk((ctX+i),(ctY-i));
+		loadChunk((ctX-i),(ctY+i));
+		loadChunk((ctX  ),(ctY+i));
+		loadChunk((ctX+i),(ctY  ));
+		loadChunk((ctX+i),(ctY+i));
+		sight[((ctX-i) + 'x' + (ctY-i))] = 1;
+		sight[((ctX  ) + 'x' + (ctY-i))] = 1;
+		sight[((ctX-i) + 'x' + (ctY  ))] = 1;
+		sight[((ctX+i) + 'x' + (ctY-i))] = 1;
+		sight[((ctX-i) + 'x' + (ctY+i))] = 1;
+		sight[((ctX  ) + 'x' + (ctY+i))] = 1;
+		sight[((ctX+i) + 'x' + (ctY  ))] = 1;
+		sight[((ctX+i) + 'x' + (ctY+i))] = 1;
+	}
+	for(var val in grounds){
+		if(!sight[val]){
+			var str = val.split('x');
+			var delx = str[0];
+			var dely = str[1];
+			console.log("deleting " + delx + dely);
+			eraseChunk(+delx, +dely);
+		}
+	}
+	water.position.x = camera.position.x;
+	water.position.z = camera.position.z;
 }
 
 function initCamera(camera){
@@ -234,7 +288,7 @@ function initCamera(camera){
 	camera.inertia=0;
 	camera.speed=20;
 	camera.angularSensibility=500;
-	camera.position.y=2000;
+	camera.position.y=100;
 }
 
 function createCanvas(){
@@ -244,8 +298,8 @@ function createCanvas(){
 	canvas.style.position = 'absolute';
 	canvas.style.top = "0px";
 	canvas.style.left = "300px";
-	canvas.width = 1000;
-	canvas.height = 1000;
+	canvas.width = 10;
+	canvas.height = 10;
 	return c;
 }
 
@@ -267,4 +321,84 @@ function biome(e) {
 		if(e<bio.value) return bio.color;
 	}
 	return 0xffffff;
+}
+
+function swim(){
+	scene.gravity.y = 0;
+	camera.speed = 50;
+	// camera.attachPostProcess(ppBlue);
+	// camera.attachPostProcess(ppBlur);
+}
+
+function walk(){
+	scene.gravity.y = -3;
+	camera.speed = 20;
+	// camera.detachPostProcess(ppBlue);
+	// camera.detachPostProcess(ppBlur);
+}
+
+function walkManager(){
+	if(camera.position.y < water.position.y+4){
+		swim();
+	} else {
+		walk();
+	}
+	if(camera.position.y < water.position.y-5){
+		if(!swimming){
+				camera.attachPostProcess(ppBlue);
+				camera.attachPostProcess(ppBlur);
+		}
+		swimming = true;
+	} else {
+		if(!swimming){
+			camera.detachPostProcess(ppBlue);
+			camera.detachPostProcess(ppBlur);
+		}
+		swimming = false;
+	}
+}
+
+function simpleGamepad(){
+	if(!Math.round(navigator.getGamepads()[0]))
+		return false;
+	var speed = camera._computeLocalCameraSpeed();
+	if(Math.round(navigator.getGamepads()[0].axes[1])==-1){
+		camera._localDirection.copyFromFloats(0, 0, -speed);
+		camera.getViewMatrix().invertToRef(camera._cameraTransformMatrix);
+		BABYLON.Vector3.TransformNormalToRef(camera._localDirection, camera._cameraTransformMatrix, camera._transformedDirection);
+		camera.cameraDirection.addInPlace(camera._transformedDirection);
+	}
+	if(Math.round(navigator.getGamepads()[0].axes[1])==1){
+		camera._localDirection.copyFromFloats(0, 0, speed);
+		camera.getViewMatrix().invertToRef(camera._cameraTransformMatrix);
+		BABYLON.Vector3.TransformNormalToRef(camera._localDirection, camera._cameraTransformMatrix, camera._transformedDirection);
+		camera.cameraDirection.addInPlace(camera._transformedDirection);
+	}
+	if(Math.round(navigator.getGamepads()[0].axes[0])==-1){
+		camera._localDirection.copyFromFloats(-speed, 0, 0);
+		camera.getViewMatrix().invertToRef(camera._cameraTransformMatrix);
+		BABYLON.Vector3.TransformNormalToRef(camera._localDirection, camera._cameraTransformMatrix, camera._transformedDirection);
+		camera.cameraDirection.addInPlace(camera._transformedDirection);
+
+	}
+	if(Math.round(navigator.getGamepads()[0].axes[0])==1){
+		camera._localDirection.copyFromFloats(speed, 0, 0);
+		camera.getViewMatrix().invertToRef(camera._cameraTransformMatrix);
+		BABYLON.Vector3.TransformNormalToRef(camera._localDirection, camera._cameraTransformMatrix, camera._transformedDirection);
+		camera.cameraDirection.addInPlace(camera._transformedDirection);
+
+	}
+
+	if(Math.round(navigator.getGamepads()[0].axes[3])==-1){
+		camera.cameraRotation.x-=0.05;
+	}
+	if(Math.round(navigator.getGamepads()[0].axes[3])==1){
+		camera.cameraRotation.x+=0.05;
+	}
+	if(Math.round(navigator.getGamepads()[0].axes[2])==-1){
+		camera.cameraRotation.y-=0.05;
+	}
+	if(Math.round(navigator.getGamepads()[0].axes[2])==1){
+		camera.cameraRotation.y+=0.05;
+	}
 }
